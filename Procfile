80b1daaa91da333f23cf80e4b62ce79dac3625b0
Nuno <nuno.m.reis@netcabo.pt>
2016-06-02 14:40:00 +0100

    Added files to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/1516-2-LI41N-G11/bin/1516-2-LI41N-G11
--- src/main/java/pt/isel/ls/movies/container/commands/common/Listen.java
+++ src/main/java/pt/isel/ls/movies/container/commands/common/Listen.java
@@ -1,9 +1,11 @@
 package pt.isel.ls.movies.container.commands.common;
 
-import org.eclipse.jetty.plus.servlet.ServletHandler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import pt.isel.ls.movies.container.commands.Command;
+import pt.isel.ls.movies.data.DataSourceFactory;
+import pt.isel.ls.movies.engine.ContextData;
 import pt.isel.ls.movies.engine.Request;
 import pt.isel.ls.movies.engine.Response;
 import pt.isel.ls.movies.engine.Router;
@@ -37,9 +39,13 @@ public class Listen extends Command {
 
         System.out.println(String.format("launching Web server at port %d...", port));
         Server server = new Server(port);
-        ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
-        handler.addServletWithMapping(new ServletHolder(Router.createRouter(dataSource).createHttpServlet(request.getContextData())), "/*");
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(Router.createRouter(dataSource).createHttpServlet(request.getContextData())), "/*");
+        //ServletHandler handler = new ServletHandler();
+        //server.setHandler(handler);
+        //handler.addServletWithMapping(new ServletHolder(Router.createRouter(dataSource).createHttpServlet(request.getContextData())), "/*");
         server.start();
         System.out.println("Server is started");
 
@@ -54,5 +60,14 @@ public class Listen extends Command {
     @Override
     public void execute(Request request, Response response) throws Exception {
         doWork(request);
+    }
+
+    public static void main(String[] args) throws Exception {
+        ContextData data = new ContextData();
+        data.dynamic = false;
+
+        String[] request = new String[]{"LISTEN", "/", "port="+System.getenv("PORT")};
+
+        new Listen(DataSourceFactory.createInstance()).doWork(Request.create(data, request));
     }
 }
